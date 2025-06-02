a37ab389dc305f1de744f49f7d67737ac60e5eb6
mariarosa <airamcmrosa@gmail.com>
2025-06-02 12:38:56 -0300

    start effect (moving)

--- js/background.js
+++ js/background.js
@@ -1,19 +1,45 @@
-
 class Star {
-    constructor() {
-        // We'll add x, y, size, speed, etc., in the next TDD cycle.
+    constructor(canvasWidth, canvasHeight) {
+        this.x = Math.random() * canvasWidth;
+        this.y = Math.random() * canvasHeight;
+        this.size = Math.random() * 2 + 1; // Size from 1 to 3
+        this.speed = Math.random() * 0.5 + 0.1; // Speed from 0.1 to 0.6
     }
+
+    update(canvasWidth) {
+        this.x += this.speed;
+        // If star goes off the right side, reset it to the left
+        if (this.x > canvasWidth) {
+            this.x = 0;
+        }
+    }
+    draw(ctx) {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+
 }
 
-
 export class Background {
-    constructor(starCount) {
-        this.stars = []; // 1. Create the stars array.
+    constructor(starCount, canvasWidth, canvasHeight) {
+        this.stars = [];
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
 
-        // 2. Loop 'starCount' times.
         for (let i = 0; i < starCount; i++) {
-            // 3. Add a new Star instance to the array.
-            this.stars.push(new Star());
+            this.stars.push(new Star(this.canvasWidth, this.canvasHeight));
         }
+    }
+    update() {
+        this.stars.forEach(star => {
+            star.update(this.canvasWidth);
+        });
+    }
+
+    // Add draw method to draw all stars
+    draw(ctx) {
+        this.stars.forEach(star => {
+            star.draw(ctx);
+        });
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+import { Background } from './background.js';
+
 window.onload = function () {
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    const starCount = 200;
+    const background = new Background(starCount, canvas.width, canvas.height);
+
+    // Create the main animation loop
+    function animate() {
+        // 1. Clear the canvas with a black background
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // 2. Update and draw the background
+        background.update();
+        background.draw(ctx);
+
+        // 3. Request the next frame
+        requestAnimationFrame(animate);
+    }
+
+    // Start the animation
+    animate();
 };
