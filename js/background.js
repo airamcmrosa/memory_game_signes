ec9be77b4d31872c72e10b254ed7876dce6c99b5
mariarosa <airamcmrosa@gmail.com>
2025-06-02 12:24:58 -0300

    start test ok

--- js/background.js
+++ js/background.js
@@ -1,3 +1,19 @@
-export class Background {
 
+class Star {
+    constructor() {
+        // We'll add x, y, size, speed, etc., in the next TDD cycle.
+    }
+}
+
+
+export class Background {
+    constructor(starCount) {
+        this.stars = []; // 1. Create the stars array.
+
+        // 2. Loop 'starCount' times.
+        for (let i = 0; i < starCount; i++) {
+            // 3. Add a new Star instance to the array.
+            this.stars.push(new Star());
+        }
+    }
 }
